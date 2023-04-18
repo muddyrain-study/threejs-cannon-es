d3850705201b9c62f46e62e08eae09e956ca81a4
“muddyrain” <qiu3291002845@gmail.com>
2023-04-18 14:16:38 +0800

    feat: 5 碰撞与碰撞组

--- src/4 弹性与接触材质详解.vue
+++ src/4 弹性与接触材质详解.vue
@@ -32,14 +32,9 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 
-// 设置碰撞组 数值 要用 2的幂
-const GROUP1 = 1
-const GROUP2 = 2
-const GROUP3 = 4
-const GROUP4 = 8
-
 const boxMaterialCon = new CANNON.Material("boxMaterial")
-boxMaterialCon.friction = 0
+boxMaterialCon.friction = 0.7
+boxMaterialCon.restitution = 1
 
 // 创建一个物理世界的平面
 const planeShape = new CANNON.Box(new CANNON.Vec3(5, 0.1, 5))
@@ -48,26 +43,24 @@ const planeBody = new CANNON.Body({
   mass: 0,
   shape: planeShape,
   material: boxMaterialCon,
-  position: new CANNON.Vec3(0, -0.1, 0),
-  type: CANNON.Body.STATIC,
-  collisionFilterGroup: GROUP1,
-  collisionFilterMask: GROUP1 | GROUP2 | GROUP3
+  position: new CANNON.Vec3(0, 0, 0),
+  type: CANNON.Body.STATIC
 
 })
-// planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), 0.1)
+planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), 0.1)
 // 将刚体添加到物理世界
 world.addBody(planeBody)
 
 
 // 创建一个平面几何体
 // const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeGeometry = new THREE.BoxGeometry(10, 0.2, 10,);
+const planeGeometry = new THREE.BoxGeometry(10, 0.2, 10);
 // 创建一个平面材质
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 // 创建一个平面网格
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 // x轴旋转90度
-// planeMesh.rotation.x = 0.1;
+planeMesh.rotation.x = 0.1;
 // 将网格添加到3D场景
 scene.add(planeMesh);
 
@@ -83,10 +76,8 @@ const boxShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
 const boxBody = new CANNON.Body({
   mass: 1,
   shape: boxShape,
-  position: new CANNON.Vec3(-2, 0.5, 0),
-  material: boxMaterialCon,
-  collisionFilterGroup: GROUP1,
-  collisionFilterMask: GROUP1 | GROUP2 | GROUP3 | GROUP4
+  position: new CANNON.Vec3(0, 5, 0),
+  material: boxMaterialCon
 })
 // 将刚体添加到物理世界
 world.addBody(boxBody)
@@ -96,55 +87,62 @@ phyMeshes.push(boxBody)
 // 创建立方体几何体
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 // 几何立方体材质
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, })
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 // 立方体物体
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
 scene.add(boxMesh)
 meshs.push(boxMesh)
 
-// 创建物理球
-const shpereShape = new CANNON.Sphere(0.5)
-// 创建物理球刚体
-const sphereBody = new CANNON.Body({
-  shape: shpereShape,
-  mass: 1,
-  position: new CANNON.Vec3(0, 0.5, 0),
-  material: boxMaterialCon,
-  collisionFilterGroup: GROUP2,
-  collisionFilterMask: GROUP1 | GROUP4
-})
-world.addBody(sphereBody)
-phyMeshes.push(sphereBody)
-// 创建几何体球
-const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32)
-// 创建球材质
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff })
-const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
-scene.add(sphereMesh)
-meshs.push(sphereMesh)
-
-// 创建物理圆柱
-const cylinerShape = new CANNON.Cylinder(0.5, 0.5, 1, 32)
+// 创建第二个物理立方体
+const boxMaterialCon2 = new CANNON.Material("boxSlipperyMaterial")
+// 设置材质摩擦系数 
+boxMaterialCon2.friction = 0
 // 创建一个刚体
-const CylinderBody = new CANNON.Body({
-  shape: cylinerShape,
+const boxBody2 = new CANNON.Body({
   mass: 1,
-  position: new CANNON.Vec3(2, 0.5, 0),
-  material: boxMaterialCon,
-  collisionFilterGroup: GROUP3,
-  collisionFilterMask: GROUP1 | GROUP4
+  shape: boxShape,
+  position: new CANNON.Vec3(2, 5, 0),
+  material: boxMaterialCon2
 })
-world.addBody(CylinderBody)
-phyMeshes.push(CylinderBody)
-// 创建圆柱几何体
-const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32)
-const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff })
-const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial)
-scene.add(cylinderMesh)
-meshs.push(cylinderMesh)
+// 将刚体添加到物理世界
+world.addBody(boxBody2)
+phyMeshes.push(boxBody2)
 
-// 设置立方体的初始速度
-boxBody.velocity.set(2, 0, 0)
+// 立方体物体
+const boxMesh2 = new THREE.Mesh(boxGeometry, boxMaterial)
+boxMesh2.position.set(2, 0, 0)
+scene.add(boxMesh2)
+meshs.push(boxMesh2)
+
+// 设置立方体材质
+const boxBouncyMaterial = new CANNON.Material("boxBouncyMaterial")
+// boxBouncyMaterial.friction = 0.1
+// boxBouncyMaterial.restitution = 1
+
+const boxBody3 = new CANNON.Body({
+  mass: 1,
+  shape: boxShape,
+  position: new CANNON.Vec3(3, 5, 0),
+  material: boxBouncyMaterial
+})
+// 将刚体添加到物理世界
+world.addBody(boxBody3)
+phyMeshes.push(boxBody3)
+
+
+// 立方体物体
+const boxMesh3 = new THREE.Mesh(boxGeometry, boxMaterial)
+boxMesh2.position.set(2, 0, 0)
+scene.add(boxMesh3)
+meshs.push(boxMesh3)
+
+// 定义接触材质
+const material3toPlane = new CANNON.ContactMaterial(boxMaterialCon, boxBouncyMaterial, {
+  friction: 0,
+  restitution: 0.1
+})
+// 将接触材质添加到物理世界
+world.addContactMaterial(material3toPlane)
 
 
 // 渲染
